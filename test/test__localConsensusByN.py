03744763867fd7a648adca25e3bab13ed12d672a
Nathan Argetsinger <nargetdev@gmail.com>
2023-12-09 17:43:24 -0800

    mentat: wrote a test case

---
+++ test/test__localConsensusByN.py
@@ -0,0 +1,22 @@
+import sys
+import toml
+sys.path.append('./src')
+from localConsensusByN import localConsensusByN
+
+def test_localConsensusByN():
+    # Read the TOML file
+    test_data = toml.load("./test/data/test_localConsensusByN.toml")
+
+    # Iterate over each test case
+    for test_case in test_data['test']:
+        buffer = test_case['INS']['buffer']
+        n = test_case['INS']['n']
+        expected_result = test_case['OUTS']['result']
+
+        # Call the function and assert the result
+        actual_result = localConsensusByN(buffer, n)
+        assert actual_result == expected_result, f"Test failed: Output {actual_result} does not match expected value {expected_result}."
+
+if __name__ == "__main__":
+    test_localConsensusByN()
+    print("\nAll tests passed: Output matches expected values.")
