e7aec4766397459f3d654fee47c7a242b27b6293
Nathan Argetsinger <nargetdev@gmail.com>
2023-12-09 17:35:15 -0800

    create prompt from committed tokens

--- src/driver.py
+++ src/driver.py
@@ -1,10 +1,11 @@
 import collections
 
 from localConsensusByN import localConsensusByN
+from overlapIndex import overlapIndex
 
 # the top level driver.  Keeps track of a minimum of state.
 
-def driver(BUFFER_LEN=4, LOCAL_AGREEMENT_N=2):
+def driver(BUFFER_LEN=4, LOCAL_AGREEMENT_N=2, PROMPT_LEN=100):
         
     # stateful vars
     
@@ -13,13 +14,23 @@ def driver(BUFFER_LEN=4, LOCAL_AGREEMENT_N=2):
     for i in range(BUFFER_LEN):
         buffer.append([])
     
+    # committed tokens so far.
+    committed_tokens = []
+    
     # for now just read from a file.  Convert this to a stream (i.e. from mqtt later)
     with open('test/data/inputs.txt', 'r') as file:
         for line in file:
             # print(line.strip())
             buffer.append(line.strip().replace('"', '').replace('\'', '').split(' '))
             
-            # run overlap index
-            # overlapIndex
+            prompt = []
+            if len(committed_tokens) > PROMPT_LEN:
+                prompt = committed_tokens[-PROMPT_LEN:]
+            else:
+                prompt = committed_tokens
             
-            newTokens = localConsensusByN(buffer, LOCAL_AGREEMENT_N)
+            candidateBuffer = [transcription[overlapIndex(prompt, transcription):] for transcription in buffer]
+            
+            newTokens = localConsensusByN(candidateBuffer, LOCAL_AGREEMENT_N)
+            
+            committed_tokens.extend(newTokens)
