bfa33cea1b113c5435c70582d7f4c3e9aed57092
Nathan Argetsinger <nargetdev@gmail.com>
2023-12-09 18:06:56 -0800

    driver emits tokens to the mosquitto topic `whisper/confirmedTokens`

--- src/driver.py
+++ src/driver.py
@@ -1,6 +1,8 @@
 import collections
 
 from localConsensusByN import localConsensusByN
+import paho.mqtt.publish as publish
+import datetime
 from overlapIndex import overlapIndex
 
 # the top level driver.  Keeps track of a minimum of state.
@@ -20,6 +22,8 @@ def driver(BUFFER_LEN=4, LOCAL_AGREEMENT_N=2, PROMPT_LEN=100):
     # for now just read from a file.  Convert this to a stream (i.e. from mqtt later)
     with open('test/data/inputs.txt', 'r') as file:
         for line in file:
+            # for each loop .. we stimulate with a `line`
+            
             # print(line.strip())
             buffer.append(line.strip().replace('"', '').replace('\'', '').split(' '))
             
@@ -34,4 +38,14 @@ def driver(BUFFER_LEN=4, LOCAL_AGREEMENT_N=2, PROMPT_LEN=100):
             
             newTokens = localConsensusByN(candidateBuffer, LOCAL_AGREEMENT_N)
             
+            # emit `newTokens` to `whisper/confirmedTokens` here.
+            # Format the current datetime as ISO 8601
+            now_iso = datetime.datetime.now().isoformat()
+            # Convert `newTokens` to a string and publish the message
+            message = f"{now_iso} {newTokens}"
+            publish.single("whisper/confirmedTokens", message, hostname="localhost")
+            
+            
             committed_tokens.extend(newTokens)
+            
+            # at the end of each loop .. we have a new committed string..
