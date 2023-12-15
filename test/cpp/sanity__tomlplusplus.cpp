db719bc697c5f1dda2ec1ebc8f8d25c081a1a767
Nathan Argetsinger <nargetdev@gmail.com>
2023-12-15 12:42:41 -0800

    this still doesn't build

--- test/cpp/sanity__tomlplusplus.cpp
+++ test/cpp/sanity__tomlplusplus.cpp
@@ -3,14 +3,16 @@
 
 int main() {
     try {
-        auto data = toml::parse("../test/data/test_localConsensusByN.toml");
+        // auto data = toml::parse("../test/data/test_localConsensusByN.toml");
+        auto data = toml::parse("../test/data/deadSimple.toml");
+        // auto data = toml::parse("../test/data/test_localConsensusByN.toml");
 
 
         // Assuming toml.hpp contains a table we can iterate over
         // This is just an example and may need to be adapted to the actual TOML structure
         for (const auto& [key, value] : *data.as_table()) {
             std::cout << "Key: " << key;
-            // std::cout << ", Value: " << value << std::endl;
+            // std::cout << value << std::endl;
         }
     } catch (const toml::parse_error& err) {
         std::cerr << "Parsing failed: " << err << std::endl;
