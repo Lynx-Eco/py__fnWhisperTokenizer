671f397e40e4c039bd5b65e8ae4439f585351c97
Nathan Argetsinger <nargetdev@gmail.com>
2023-12-15 13:17:04 -0800

    ok ditch this .. back to the drawing board.  I need a simpler and typesafe approach to reading these tomls

--- test/cpp/test__localConsensusByN.cpp
+++ test/cpp/test__localConsensusByN.cpp
@@ -3,64 +3,69 @@
 #include <cassert>
 #include <string>
 #include "localConsensusByN.hpp" // Assuming the function is declared in this header
-#include <cpptoml.h>
 
+// #include "../../tomlplusplus/examples/examples.hpp"
+#include <toml++/toml.hpp>
+
+#include <string_view>
+
+using namespace std::string_view_literals;
 
 void test_localConsensusByN() {
-    // auto config = cpptoml::parse_file("./test/data/test_localConsensusByN.toml");
-    auto config = cpptoml::parse_file("../test/data/test_localConsensusByN.toml");
-    auto tests = config->get_table_array("test");
+    try {
+        auto config = toml::parse_file("../test/data/test_localConsensusByN.toml");
+        auto tests = config["test"].as_array();
 
-    for (const auto& test_table : *tests) {
-        auto ins = test_table->get_table("INS");
-        auto outs = test_table->get_table("OUTS");
+        for (auto& test_table : *tests) {
+            toml::table ins = *test_table["INS"sv].as_table();
+            toml::table outs = *test_table["OUTS"sv].as_table();
 
-        std::vector<std::vector<std::string>> buffer;
-        auto buffer_array = ins->get_array_of<cpptoml::array>("buffer");
-        if (buffer_array) {
-            for (const auto& line : *buffer_array) {
+            std::vector<std::vector<std::string>> buffer;
+            for (const auto& line : *ins["buffer"sv].as_array()) {
                 std::vector<std::string> line_data;
-                for (const auto& value : *line) {
-                    line_data.push_back(value->as<std::string>()->get());
+                for (const auto& value : line.as_array()) {
+                    line_data.push_back(value.value_or(""sv));
                 }
                 buffer.push_back(line_data);
             }
-        } else {
-            std::cerr << "Error reading buffer from TOML file." << std::endl;
-        }
-        int n = *ins->get_as<int>("n");
-        std::vector<std::string> expected_result;
-        for (const auto& value : *outs->get_array_of<std::string>("result")) {
-            expected_result.push_back(value);
-        }
+            int n = ins["n"sv].value_or(0);
+            std::vector<std::string> expected_result;
+            for (const auto& value : outs["result"sv].as_array()) {
+                expected_result.push_back(value.value_or(""sv));
+            }
+            
 
-        std::vector<std::string> actual_result = localConsensusByN(buffer, n);
+            std::vector<std::string> actual_result = localConsensusByN(buffer, n);
 
-        std::cout << "\n================================" << std::endl;
-        std::cout << "INPUTS" << std::endl;
-        std::cout << "------" << std::endl;
-        std::cout << "buffer: ";
-        for (const auto& line : buffer) {
-            for (const auto& val : line) {
+            std::cout << "\n================================" << std::endl;
+            std::cout << "INPUTS" << std::endl;
+            std::cout << "------" << std::endl;
+            std::cout << "buffer: ";
+            for (const auto& line : buffer) {
+                for (const auto& val : line) {
+                    std::cout << val << " ";
+                }
+                std::cout << std::endl;
+            }
+            std::cout << "n: " << n << std::endl;
+            std::cout << "OUTPUTS" << std::endl;
+            std::cout << "-------" << std::endl;
+            std::cout << "expected_result: ";
+            for (const auto& val : expected_result) {
+                std::cout << val << " ";
+            }
+            std::cout << "\n-----------------------------" << std::endl;
+            std::cout << "actual_result: ";
+            for (const auto& val : actual_result) {
                 std::cout << val << " ";
             }
             std::cout << std::endl;
-        }
-        std::cout << "n: " << n << std::endl;
-        std::cout << "OUTPUTS" << std::endl;
-        std::cout << "-------" << std::endl;
-        std::cout << "expected_result: ";
-        for (const auto& val : expected_result) {
-            std::cout << val << " ";
-        }
-        std::cout << "\n-----------------------------" << std::endl;
-        std::cout << "actual_result: ";
-        for (const auto& val : actual_result) {
-            std::cout << val << " ";
-        }
-        std::cout << std::endl;
 
-        assert(actual_result == expected_result && "Test failed: Output does not match expected value.");
+            assert(actual_result == expected_result && "Test failed: Output does not match expected value.");
+        }
+    } catch (const toml::parse_error& err) {
+        std::cerr << "TOML parse error: " << err << std::endl;
+        exit(1);
     }
 }
 
