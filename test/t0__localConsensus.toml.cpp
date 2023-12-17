fe12acd90c6de4974596fa24e8f8a48e27fe1547
Nathan Argetsinger <nargetdev@gmail.com>
2023-12-17 12:02:52 -0800

    more readable

--- test/t0__localConsensus.toml.cpp
+++ test/t0__localConsensus.toml.cpp
@@ -6,34 +6,48 @@
 
 int main() {
     try {
+#include "../src/cpp/localConsensusByN.hpp"
+#include "../tomlplusplus/include/toml++/toml.hpp"
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main() {
+    try {
         auto data = toml::parse_file("test/data/localConsensusByN/t0__localConsensus.toml");
         auto buffer = data["buffer"].as_array();
         int64_t n = data["n"].value_or(-777);
 
-        std::vector<std::vector<std::string>> bufferVec;
+        vector<vector<string>> bufferVec;
         for (const auto& line : *buffer) {
-            std::vector<std::string> lineVec;
+            vector<string> lineVec;
             for (const auto& word : *line.as_array()) {
                 lineVec.push_back(word.as_string().str);
             }
             bufferVec.push_back(lineVec);
         }
 
-        std::vector<std::string> result = localConsensusByN(bufferVec, n);
-        std::vector<std::string> expected = data["return"].as_array()->as_vector<std::string>();
+        vector<string> result = localConsensusByN(bufferVec, n);
+        vector<string> expected = data["return"].as_array()->as_vector<string>();
 
         bool success = result == expected;
-        std::cout << "Test " << (success ? "PASSED" : "FAILED") << ": ";
+        cout << "Test " << (success ? "PASSED" : "FAILED") << ": ";
         for (const auto& word : result) {
-            std::cout << word << ' ';
+            cout << word << ' ';
         }
-        std::cout << std::endl;
+        cout << endl;
     } catch (const toml::parse_error& err) {
-        std::cerr << "Parsing failed: " << err << std::endl;
+        cerr << "Parsing failed: " << err << endl;
         return 1;
-    } catch (const std::exception& err) {
-        std::cerr << "Unhandled exception: " << err.what() << std::endl;
+    } catch (const exception& err) {
+        cerr << "Unhandled exception: " << err.what() << endl;
         return 1;
+    }
+
+    return 0;
+}
     }
 
     return 0;
