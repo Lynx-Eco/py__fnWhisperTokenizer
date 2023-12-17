9b4ae7ac74fe8c2415d9c96de0d82a524951a625
Nathan Argetsinger <nargetdev@gmail.com>
2023-12-17 12:17:10 -0800

    struggling to parse out this toml with toml++

--- test/t0__localConsensus.toml.cpp
+++ test/t0__localConsensus.toml.cpp
@@ -8,28 +8,31 @@ using namespace std;
 
 int main() {
     try {
-        auto data = toml::parse_file("test/data/localConsensusByN/t0__localConsensus.toml");
+        // auto data = toml::parse_file("test/data/localConsensusByN/t0__localConsensus.toml");
+        auto data = toml::parse_file("../test/data/localConsensusByN/t0__localConsensus.toml");
         auto buffer = data["buffer"].as_array();
         int64_t n = data["n"].value_or(-777);
 
+        cout << n;
         vector<vector<string>> bufferVec;
         for (const auto& line : *buffer) {
             vector<string> lineVec;
             for (const auto& word : *line.as_array()) {
-                lineVec.push_back(word.as_string().str);
+                cout << "wut" << endl;
+                // lineVec.push_back(word.as_string().str);
             }
             bufferVec.push_back(lineVec);
         }
 
-        vector<string> result = localConsensusByN(bufferVec, n);
-        vector<string> expected = data["return"].as_array()->as_vector<string>();
+        // vector<string> result = localConsensusByN(bufferVec, n);
+        // vector<string> expected = data["return"].as_array()->as_vector<string>();
 
-        bool success = result == expected;
-        cout << "Test " << (success ? "PASSED" : "FAILED") << ": ";
-        for (const auto& word : result) {
-            cout << word << ' ';
-        }
-        cout << endl;
+        // bool success = result == expected;
+        // cout << "Test " << (success ? "PASSED" : "FAILED") << ": ";
+        // for (const auto& word : result) {
+        //     cout << word << ' ';
+        // }
+        // cout << endl;
     } catch (const toml::parse_error& err) {
         cerr << "Parsing failed: " << err << endl;
         return 1;
